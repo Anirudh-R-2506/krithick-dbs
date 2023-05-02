3551e2aa0fa501286cfe87fd15bce244c9f0e6ba
Anirudh R <anirudhnfs01@gmail.com>
2023-05-02 13:31:42 +0530

    New beginnings

---
+++ history.sql
@@ -0,0 +1,23 @@
+set serveroutput on
+set verify off
+DECLARE
+    p_custID CUSTOMER.CustID%TYPE;
+    p_name CUSTOMER.name%TYPE;
+BEGIN
+    DBMS_OUTPUT.PUT_LINE('Donation history for ' || p_name || ':');
+    FOR rec IN (SELECT name, volume, blood_bank, date_of_donation, time_of_donation FROM donor WHERE id = p_custID AND name = p_name)
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Donated ' || rec.volume || ' ml of blood to ' || rec.blood_bank || ' on ' || rec.date_of_donation);
+    END LOOP;
+    DBMS_OUTPUT.PUT_LINE('Receiving history for ' || p_name || ':');
+    FOR rec IN (SELECT name, volume, blood_bank, date_of_receiving, time_of_receiving FROM receiver WHERE id = p_custID AND name = p_name)
+    LOOP
+        DBMS_OUTPUT.PUT_LINE('Received ' || rec.volume || ' ml of blood from ' || rec.blood_bank || ' on ' || rec.date_of_receiving);
+    END LOOP;
+    EXCEPTION
+        WHEN NO_DATA_FOUND THEN
+            DBMS_OUTPUT.PUT_LINE(' User does not exist ');
+        WHEN OTHERS THEN
+            DBMS_OUTPUT.PUT_LINE(' Error ');
+END;
+/
